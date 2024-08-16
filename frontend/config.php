0de9c35fe3cce18e3f5d965e44404b170c956d43
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-08-15 23:43:36 -0300

    usuário

--- frontend/config.php
+++ frontend/config.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $servername = "localhost"; // Alterar se necessário
-$username = "lucas"; // Alterar se necessário
+$username = "estudante"; // Alterar se necessário
 $password = "2022#estudante"; // Alterar se necessário
 $dbname = "BD_Trabalho"; // Alterar se necessário
 
