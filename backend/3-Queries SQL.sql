6852908a867676ed18251f676e09bcc82bdd85fb
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-08-17 19:04:45 -0300

    novas queries

--- backend/3-Queries SQL.sql
+++ backend/3-Queries SQL.sql
@@ -1,13 +1,5 @@
 use bd_trabalho;
 
-SHOW DATABASES;
-
-SELECT user, host FROM mysql.user WHERE user = 'lucas';
-
-CREATE USER 'lucas'@'localhost' IDENTIFIED BY '2022#estudante';
-GRANT ALL PRIVILEGES ON BD_Trabalho.* TO 'lucas'@'localhost';
-FLUSH PRIVILEGES;
-
 -- Calcula a porcentagem de orçamentos feitos que geraram pelo menos um pedido de venda
 SELECT 
     CONCAT(
@@ -44,3 +36,26 @@ SELECT CONCAT('R$ ', ROUND(
         (SELECT SUM(precoCotado) FROM pedidovendaorc p JOIN orcvenda o ON p.OrcVenda_dataCotacao = o.dataCotacao AND p.OrcVenda_Pessoa_idPessoa = o.Pessoa_idPessoa)
     ) / COUNT(*), 2)) AS ValorTicketMedio
 FROM pedidovenda;
+
+-- Seleciona o nome, o número de registro e a quantidade de pedidos vendidos dos vendedores
+-- usamos para conseguir verificar se exite para fazer a conexão com o php
+SELECT user, host FROM mysql.user WHERE user = 'estudante';
+
+-- Esta consulta recupera os nomes dos vendedores e seus números de telefone, combinando as 
+-- tabelas Vendedor, PessoaFisica, e PessoaFone, e ordena os resultados alfabeticamente pelo 
+-- nome do vendedor.
+SELECT PessoaFisica.nome, PessoaFone.fone
+FROM Vendedor
+JOIN PessoaFisica ON Vendedor.PessoaFisica_Pessoa_idPessoa = PessoaFisica.Pessoa_idPessoa
+JOIN PessoaFone ON PessoaFisica.Pessoa_idPessoa = PessoaFone.Pessoa_idPessoa
+ORDER BY PessoaFisica.nome ASC;
+
+--Esta consulta agrupa os produtos pelo tipo de material, calcula a quantidade total em estoque 
+-- para cada material, e exibe apenas os materiais que têm mais de 5 unidades em estoque.
+SELECT 
+    Produto.material, 
+    SUM(Produto.qtdeEstoque) AS total_estoque
+FROM Produto
+GROUP BY Produto.material
+HAVING SUM(Produto.qtdeEstoque) > 5
+ORDER BY total_estoque DESC;
