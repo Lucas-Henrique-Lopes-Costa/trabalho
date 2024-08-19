9054ccd85bd0bd6441c51f5d633949918e339bdb
gustavo <tteodorogustavo@gmail.com>
2024-08-18 21:13:26 -0300

    adicionando minhas Queries

--- backend/3-Queries SQL.sql
+++ backend/3-Queries SQL.sql
@@ -44,3 +44,37 @@ SELECT CONCAT('R$ ', ROUND(
         (SELECT SUM(precoCotado) FROM pedidovendaorc p JOIN orcvenda o ON p.OrcVenda_dataCotacao = o.dataCotacao AND p.OrcVenda_Pessoa_idPessoa = o.Pessoa_idPessoa)
     ) / COUNT(*), 2)) AS ValorTicketMedio
 FROM pedidovenda;
+
+-- Seleciona o número de telefone de cada Pessoa que é cliente e seu nome
+SELECT PFIS.nome AS NomeCliente, PF.fone AS TelefoneCliente
+FROM PessoaFisica PFIS JOIN PessoaFone PF
+ON PFIS.Pessoa_idPessoa = PF.Pessoa_idPessoa; 
+
+-- Seleciona e ordena os clientes que fizeram um Orçamento de venda e do que mais fez para o que menos fez
+SELECT PF.nome AS NomeCliente, COUNT(OV.Pessoa_idPessoa) AS QuantidadeDeOrcamentos
+FROM PessoaFisica PF JOIN OrcVenda OV
+ON PF.Pessoa_idPessoa = OV.Pessoa_idPessoa
+GROUP BY NomeCliente
+ORDER BY QuantidadeDeOrcamentos DESC;
+
+-- Seleciona os dados do Número de registro do vendedor, e a quantidade de pedidos de venda feitas por ele
+SELECT V.numRegistro AS NumeroRegistro, COUNT(PV.Vendedor_PessoaFisica_Pessoa_idPessoa) AS NumeroPedidos
+FROM Vendedor V JOIN PedidoVenda PV 
+ON PV.Vendedor_PessoaFisica_Pessoa_idPessoa = V.PessoaFisica_Pessoa_idPessoa
+GROUP BY V.numRegistro;
+
+--seleciona a quantidade de clientes que vieram por cada anuncio - GROUP BY + count
+SELECT idAnuncio, COUNT(PessoaFisica_Pessoa_idPessoa) AS quantidadeClientes
+FROM Lead
+GROUP BY idAnuncio;
+
+-- selecionar todos os clientes que se cadastraram entre duas datas específicas - BETWEEN
+SELECT PessoaFisica_Pessoa_idPessoa, idAnuncio, dataCadastro
+FROM Lead
+WHERE dataCadastro BETWEEN '2024-01-01' AND '2024-06-30';
+
+-- agrupa por preço médio das marcas
+SELECT marca, AVG(precoVenda) AS precoMedio
+FROM Produto
+WHERE marca IN ('%A', '%B', '%C')
+GROUP BY marca;
